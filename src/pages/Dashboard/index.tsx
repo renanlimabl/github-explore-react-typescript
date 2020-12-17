11426113b3d109bbdf80071b28c9711773adfab5
renanlimabl <renanlimabl@gmail.com>
2020-12-17 03:55:02 -0300

    Add repo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 import logo from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logo} alt="Github Explore" />
-    <Title>Explore repositórios no Github</Title>
-    <Form action="">
-      <input placeholder="Digite o repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
-    <Repositories>
-      <a href="/">
-        <img
-          src="https://cdn.discordapp.com/avatars/626612399318761484/eb14d117157e56c2e9f05ca597be087b.png?size=512"
-          alt="renan"
+// apenas o que vamos utilizar
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositores] = useState<Repository[]>(() => {
+    const storagedRepositores = localStorage.getItem(
+      '@githubExplore:repositories',
+    );
+
+    if (storagedRepositores) {
+      return JSON.parse(storagedRepositores);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@githubExplore:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(event: FormEvent): Promise<void> {
+    event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
+      setRepositores([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório.');
+    }
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Github Explore" />
+      <Title>Explore repositórios no Github</Title>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o repositório"
         />
-        <div>
-          <strong>Renan Lima</strong>
-          <p>Description que vai ta no repositório ReactJS, NodeJS.</p>
-        </div>
+        <button type="submit">Pesquisar</button>
+      </Form>
+      {inputError && <Error>{inputError}</Error>}
+      <Repositories>
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="/">
-        <img
-          src="https://cdn.discordapp.com/avatars/626612399318761484/eb14d117157e56c2e9f05ca597be087b.png?size=512"
-          alt="renan"
-        />
-        <div>
-          <strong>Renan Lima</strong>
-          <p>Description que vai ta no repositório ReactJS, NodeJS.</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-      <a href="/">
-        <img
-          src="https://cdn.discordapp.com/avatars/626612399318761484/eb14d117157e56c2e9f05ca597be087b.png?size=512"
-          alt="renan"
-        />
-        <div>
-          <strong>Renan Lima</strong>
-          <p>Description que vai ta no repositório ReactJS, NodeJS.</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,9 @@
-import styled from 'styled-components';
+/* eslint-disable prettier/prettier */
+import styled, { css } from 'styled-components';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -8,7 +13,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -20,6 +25,14 @@ export const Form = styled.form`
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
     padding: 0 24px;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${props =>
+    props.hasError &&
+    css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -71,7 +84,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      margin: 0 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
@@ -89,4 +103,10 @@ export const Repositories = styled.div`
       color: #cbcbd6;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 30px;
 `;
